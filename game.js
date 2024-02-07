7bb7da996747f4ae815564e1a2fd19059cfe2bf5
volynets-vlad <158173204+volynets-vlad@users.noreply.github.com>
2024-02-07 14:52:16 +0200

    крок 7, крок 8

--- game.js
+++ game.js
@@ -17,7 +17,9 @@ var config = {
 };
 
 var player;
+var stars;
 var platforms;
+var cursors;
 
 var game = new Phaser.Game(config);
 
@@ -67,9 +69,54 @@ function create ()
         repeat: -1
     });
 
+    cursors = this.input.keyboard.createCursorKeys();
+
+    stars = this.physics.add.group({
+        key: 'star',
+        repeat: 11,
+        setXY: { x: 12, y: 0, stepX: 70 }
+    });
+
+    stars.children.iterate(function (child) {
+
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+
+    });
+
     this.physics.add.collider(player, platforms);
+    this.physics.add.collider(stars, platforms);
+
+    this.physics.add.overlap(player, stars, collectStar, null, this);
 }
 
 function update ()
 {
+    if (cursors.left.isDown)
+    {
+        player.setVelocityX(-160);
+
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(160);
+
+        player.anims.play('right', true);
+    }
+    else
+    {
+        player.setVelocityX(0);
+
+        player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.setVelocityY(-330);
+    }
+}
+
+function collectStar (player, star)
+{
+    star.disableBody(true, true);
 }
