21ab08a7262a396c27eddec15a0b09f31f85e0c0
volynets-vlad <158173204+volynets-vlad@users.noreply.github.com>
2024-02-07 14:55:14 +0200

    крок 9

--- game.js
+++ game.js
@@ -20,6 +20,8 @@ var player;
 var stars;
 var platforms;
 var cursors;
+var score = 0;
+var scoreText;
 
 var game = new Phaser.Game(config);
 
@@ -83,6 +85,8 @@ function create ()
 
     });
 
+    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+
     this.physics.add.collider(player, platforms);
     this.physics.add.collider(stars, platforms);
 
@@ -119,4 +123,7 @@ function update ()
 function collectStar (player, star)
 {
     star.disableBody(true, true);
+
+    score += 10;
+    scoreText.setText('Score: ' + score);
 }
